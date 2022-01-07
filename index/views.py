e261fdd32296ba1c34d087170a603cb21a1b8668
Addd12 <addd.github@gmail.com>
2022-01-07 04:35:38 +0200

    + user data and styled profile

--- index/views.py
+++ index/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-# def index(request):
-#     return render(request, "index/index.html")
+def about(request):
+    return render(request, "index/about.html")
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import redirect, render
-#from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
+from users.forms import UserUpdateForm
 #from django.views.decorators.cache import cache_control
 
 
@@ -24,4 +26,20 @@ def register(request):
 #@cache_control(must_revalidate = True)
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your profile has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
